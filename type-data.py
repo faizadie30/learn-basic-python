887e99fe5fe6b3ca33bdf097a309c3da1f786c6d
faizadie30 <faiz.adie30@gmail.com>
2023-05-12 00:38:33 +0700

    type data on python

---
+++ type-data.py
@@ -0,0 +1,32 @@
+#type data boolean
+print("this boolean data:",True)
+
+#type data integer
+print("this boolean data:", 3)
+
+#type data float
+print(3.14)
+
+#type data string
+print("this string type data")
+
+#type data Hexadecimal
+# print(9a)
+
+#type data Complex
+print(5j)
+
+#type data array or list
+print(['hallo',"world"])
+print([1,2,3,4,5])
+
+#type data Tuple
+print((1,2,3,4,5))
+print(("satu", "dua", "tiga"))
+
+#tipe data Dictionary
+print({"nama":"Budi", 'umur':20})
+#tipe data Dictionary dimasukan ke dalam variabel biodata
+biodata = {"nama":"Andi", 'umur':21} #proses inisialisasi variabel biodata
+print(biodata) #proses pencetakan variabel biodata yang berisi tipe data Dictionary
+print(type(biodata)) #fungsi untuk mengecek jenis tipe data. akan tampil <class 'dict'> yang berarti dict adalah tipe data dictionary
