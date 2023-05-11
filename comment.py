28d76ed61da2bf6df8036371161fcce3c8d0deaf
faizadie30 <faiz.adie30@gmail.com>
2023-05-12 00:12:47 +0700

    example comment

---
+++ comment.py
@@ -0,0 +1,24 @@
+#This a comment
+
+# in text can't execute
+
+#comment with 1 hastag support comment 1 line
+
+"""
+if much comment by line use string two as much thrice
+lorem10
+lorem10000
+lorem203942
+"""
+
+print("Hello World") #this comment and flexible position
+
+#print("Welcome")
+
+# comment support special character !@#$%^&\*(),./;'[]\
+
+#print name bambang
+print("Bambang")
+
+#print number
+print(123)
