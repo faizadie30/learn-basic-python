154774d0341b3cc4bb526d6411042061fab1b6a3
faizadie30 <faiz.adie30@gmail.com>
2023-05-14 10:29:01 +0700

    example import file to another file

---
+++ example_import_math_logic/index.py
@@ -0,0 +1,7 @@
+import logic_math
+
+addition = logic_math.addition(5,10,25,5)
+print(f"result 5 + 10 + 25 + 5 = {addition}")
+
+substraction = logic_math.substraction(100, 25)
+print(f"result 100 - 25 = {substraction}")
---
+++ example_import_math_logic/logic_math.py
@@ -0,0 +1,8 @@
+def addition(*args):
+    result = 0
+    for valueI in args:
+        result += valueI
+    return result
+
+def substraction(a,b):
+    return a - b
